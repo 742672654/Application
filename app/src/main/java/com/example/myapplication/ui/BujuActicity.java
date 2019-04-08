00783ceebdbf872448067708e309f932a75c0cea
xiaoduo <xiaoduo>
2019-04-08 09:33:19 +0800

    上传

---
+++ app/src/main/java/com/example/myapplication/ui/BujuActicity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.myapplication.R;
+
+public class BujuActicity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onCreate(Bundle v) {
+        super.onCreate(v);
+        setContentView(R.layout.linear_layout);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/RelativeLayoutActicity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.myapplication.R;
+
+public class RelativeLayoutActicity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle v) {
+        super.onCreate(v);
+        setContentView( R.layout.relative_layout);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+}
